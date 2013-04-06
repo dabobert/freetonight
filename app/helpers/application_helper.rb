e9c7ca8d233d272d1acaaea60aea7f2703aed504
Robert Jenkins <bobert@gmail.com>
2013-04-06 13:39:14 -0400

    - turned hours into a helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def hours(label=false)
+    content_tag :div, class: "hours" do
+      (0..23).step(0.5).to_a.collect do |i|
+        "<div class=\"half_hour\">#{(Time.utc(0)+i.hour).strftime("%-I%P") if (i/ 0.5) % 2 == 0 && label}</div>".html_safe
+        #"<div class=\"half_hour\">&nbsp;</div>".html_safe
+        #content_tag :div, :class => "half_hour"
+      end.reduce(:<<) # Will concat using the SafeBuffer instead of String with join
+    end
+  end
 end
