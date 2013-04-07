ca9764f74b517d83fc90ac411a987f18f389a417
Robert Jenkins <bobert@gmail.com>
2013-04-06 21:03:40 -0400

    - finally got the weekly calendar styled correctly

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,16 @@ module ApplicationHelper
 
   def hours(label=false)
     content_tag :div, class: "hours" do
-      (0..23).step(0.5).to_a.collect do |i|
-        "<div class=\"half_hour\">#{(Time.utc(0)+i.hour).strftime("%-I%P") if (i/ 0.5) % 2 == 0 && label}</div>".html_safe
+      if label.present?
+        inc = 1
+        css = "hour"
+      else
+        inc = 0.5
+        css = "half_hour"
+      end
+
+      (0..23.5).step(inc).to_a.collect do |i|
+        "<div class=\"#{css}\">#{(Time.utc(0)+i.hour).strftime("%-I%P") if (i/ 0.5) % 2 == 0 && label}</div>".html_safe
         #"<div class=\"half_hour\">&nbsp;</div>".html_safe
         #content_tag :div, :class => "half_hour"
       end.reduce(:<<) # Will concat using the SafeBuffer instead of String with join
