619693d7c067bb77ed689bfa1ef7265b517aa22c
Robert Jenkins <bobert@gmail.com>
2013-04-14 16:05:44 -0400

    - removed useless lines from home controller

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,8 +1,7 @@
 class HomesController < ApplicationController
 
   def show
-    @date = Time.now.to_date
-    @articles =[]
-    @articles_by_date=[]
+    @availability     = Availability.first
+    @date             = Time.now.to_date
   end
 end
