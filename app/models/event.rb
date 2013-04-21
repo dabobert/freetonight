15273dbd01fa45ce34830836e877a65c38a8082c
Robert Jenkins <bobert@gmail.com>
2013-04-21 15:07:01 -0400

    - had to fix the Event.schedule function to use YAML::load

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ActiveRecord::Base
   include IceCube
   serialize :schedule, Hash
 
-  attr_accessible :duration, :frequency
+  attr_accessible :duration, :frequency, :name, :schedule
 
   def schedule=(new_schedule)
     @schedule = new_schedule
@@ -10,7 +10,7 @@ class Event < ActiveRecord::Base
   end
 
   def schedule
-    @schedule ||= Schedule.from_hash(read_attribute(:schedule_text))
+    @schedule ||= Schedule.from_hash(YAML::load read_attribute(:schedule_text))
   end
 
 end
